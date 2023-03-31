515f512330f3a0b2f92eca470d44f3b371210778
astro <astroharpy@outlook.com>
2023-03-31 17:41:00 +0200

    test

--- server/app.js
+++ server/app.js
@@ -1,10 +1,10 @@
-require("dotenv").config()
+
 const app = require("express")()
 
 const PORT = 8000
 
 app.get('/', (req, res)=>{
-    res.send(`<h1>Welcome ${process.env.ICE} </h1>`)
+    res.send(`<h1>Welcome ${process.env.ice} </h1>`)
 })
 
 app.listen(PORT, ()=> console.log(`The app is running on port : ${PORT}`))
