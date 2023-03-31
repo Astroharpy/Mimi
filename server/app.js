febd31210607181c4f6595d94f3e273da3cda645
astro <astroharpy@outlook.com>
2023-03-31 17:29:58 +0200

    test

--- server/app.js
+++ server/app.js
@@ -1,70 +1,10 @@
-require("dotenv").config();
-const express = require("express")
-const app = express()
-const PORT = process.env.PORT || 8000
-const credentials = require('./middleware/credentials');
-const corsOptions = require('./config/corsOption');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const admin = require('./routes/admin')
-const mongoose = require('mongoose')
-const connectDB = require('./config/dbConn')
+require("dotenv").config()
+const app = require("express")()
 
-//connects to mongoDB
-connectDB()
+const PORT = 8000
 
-const Product = require('./model/Product')
-
-//middleware
-app.use(credentials);
-app.use(cors(corsOptions));
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-//admin route
-app.use(admin)
-
-//Home route
-// app.get('/',(req,res)=>{
-//     res.status(200).json( [
-//         {price:100,name:"first"},
-//         {price:100,name:"second"},
-//         {price:100,name:"third"},
-//         {price:100,name:"fourth"},
-//         {price:100,name:"fifth"},
-//         {price:100,name:"sixth"},
-//         {price:100,name:"seventh"},
-//         {price:100,name:"eighth"},
-//         {price:100,name:"ninth"},
-//         {price:100,name:"tenth"}
-    
-//         ])
-// })
-
-app.get('/', async (req,res)=>{
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        
-    }
-    
+app.get('/', (req, res)=>{
+    res.send(`<h1>Welcome ${process.env.ICE} </h1>`)
 })
 
-
-
-app.all("*",(req,res)=>{
-    res.status(404).send("Not found")
-})
-
-//connecting to the database
-mongoose.connection.once('open', ()=>{
-    //starting the server
-    app.listen(PORT, ()=>{
-        console.log(`The Server is running on port : ${PORT}`);
-    })
-})
-
-
-
+app.listen(PORT, ()=> console.log(`The app is running on port : ${PORT}`))
