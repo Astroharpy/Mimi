9a3c4c6b51c573169f274a927a914cf0f31958b7
astro <astroharpy@outlook.com>
2023-03-31 17:59:27 +0200

    test

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = require("express")()
 const PORT = 3000
 
 app.get('/', (req, res)=>{
-    res.send(`<h1>Welcome ${process.env.ice} </h1>`)
+    res.send(`<h1>Welcome ice </h1>`)
 })
 
 app.listen(PORT, ()=> console.log(`The app is running on port : ${PORT}`))
